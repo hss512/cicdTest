c6f32b89bfe634b660ddbf00b4ce4fc58cddeb79
Seong su Ha <hss25350@naver.com>
2022-07-18 14:36:32 +0900

    test api

--- src/main/java/com/study/cicdtest/api/TestApiController.java
+++ src/main/java/com/study/cicdtest/api/TestApiController.java
@@ -3,14 +3,14 @@ package com.study.cicdtest.api;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
 public class TestApiController {
 
-    @PostMapping("/please")
+    @GetMapping("/please")
     public ResponseEntity<?> test(){
 
         log.info("Please....");
