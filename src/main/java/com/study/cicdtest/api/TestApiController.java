c47daea0324b053f6355ef1bc85e8edd0ddf4fc4
Seong su Ha <hss25350@naver.com>
2022-07-18 13:50:41 +0900

    test api

--- src/main/java/com/study/cicdtest/api/TestApiController.java
+++ src/main/java/com/study/cicdtest/api/TestApiController.java
@@ -1,4 +1,16 @@
 package com.study.cicdtest.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestApiController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> test(){
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
